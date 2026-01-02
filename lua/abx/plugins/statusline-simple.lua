d318798287c36e30bf393cd6397370d110767493
Abinash <abixnash@gmail.com>
2026-01-03 02:24:37 +0530

    deleted unwanted code

--- lua/abx/plugins/statusline-simple.lua
+++
@@ -1,7 +0,0 @@
--- Simple statusline test
-vim.opt.statusline = "%f test%y"
-vim.opt.laststatus = 2
-
-print("Basic statusline test:")
-print("Statusline option:", vim.opt.statusline)
-print("Laststatus:", vim.opt.laststatus)
