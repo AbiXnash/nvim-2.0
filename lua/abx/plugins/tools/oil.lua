40c48248d2e3ad96c4b7c457d3d9913ebc948224
Abinash <abixnash@gmail.com>
2026-01-22 22:41:14 +0530

    feat(oil): enhance file explorer configuration
    - Configure oil to behave more like netrw (buflisted, concealcursor)
    - Add float window options for popup mode
    - Add leader keybindings for float and regular oil explorer
    - Enable skip_confirm_for_simple_edits

--- lua/abx/plugins/tools/oil.lua
+++ lua/abx/plugins/tools/oil.lua
@@ -1,4 +1,4 @@
--- File manager (oil.nvim)
+-- File manager (oil.nvim) - configured to look like netrw
 return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -6,7 +6,7 @@ return {
         require("oil").setup({
             columns = { "icon" },
             buf_options = {
-                buflisted = false,
+                buflisted = true,
                 bufhidden = "hide",
             },
             win_options = {
@@ -17,11 +17,11 @@ return {
                 spell = false,
                 list = false,
                 conceallevel = 3,
-                concealcursor = "n",
+                concealcursor = "nvic",
             },
             default_file_explorer = true,
             restore_win_options = true,
-            skip_confirm_for_simple_edits = false,
+            skip_confirm_for_simple_edits = true,
             watch_for_changes = false,
             popup_border = "single",
             cleanup_delay_ms = 2000,
@@ -47,9 +47,27 @@ return {
                 ["g."] = "actions.toggle_hidden",
                 ["//"] = "actions.toggle_trash",
             },
+            view_options = {
+                show_numbers = false,
+                show_hidden = true,
+            },
+            float = {
+                padding = 2,
+                max_width = 0,
+                max_height = 0,
+                border = nil,
+                win_options = {
+                    winblend = 0,
+                },
+                get_win_title = nil,
+                preview_split = "auto",
+                override = function(conf)
+                    return conf
+                end,
+            },
         })
 
-        vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Oil file explorer" })
+        vim.keymap.set("n", "<leader>pe", "<cmd>Oil --float<CR>", { desc = "Oil file explorer(Float)" })
         vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>", { desc = "Oil file explorer" })
     end,
 }
